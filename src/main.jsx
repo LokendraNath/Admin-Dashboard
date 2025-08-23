8910e05e046c8ba566ac7ae0cd8b4824c5ce7862
Lokendra Nath <nathdeveloper25@gmail.com>
2025-08-23 18:36:15 +0530

    route add for sidebar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Layout from "./components/Layout/Layout.jsx";
+import Dashboard from "./components/Dashboard/Dashboard.jsx";
 
-createRoot(document.getElementById('root')).render(
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import Profile from "./pages/Profile.jsx";
+import { Transection } from "./pages/Transection.jsx";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+      },
+      {
+        path: "transection",
+        element: <Transection />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={routes} />
+  </StrictMode>
+);
