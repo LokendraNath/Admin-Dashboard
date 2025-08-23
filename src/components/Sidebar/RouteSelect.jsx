285ab601606a07995a10b420e05c8e4c4bcb45fb
Lokendra Nath <nathdeveloper25@gmail.com>
2025-08-23 18:38:50 +0530

    add some routes and fix error

--- src/components/Sidebar/RouteSelect.jsx
+++ src/components/Sidebar/RouteSelect.jsx
@@ -3,35 +3,42 @@ import {
   ChartBarSquareIcon,
   ClipboardDocumentCheckIcon,
   CreditCardIcon,
+  ShoppingBagIcon,
   UserCircleIcon,
   UserGroupIcon,
   UserIcon,
 } from "@heroicons/react/16/solid";
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const RouteSelect = () => {
   return (
     <div className="space-y-5 p-4">
-      <Route selected={true} icon="ChartBarSquare" iconName="Dashboard" />
-      <Route selected={false} icon="Banknotes" iconName="Transection" />
-      <Route selected={false} icon="UserIcon" iconName="Profile" />
+      <Route icon="ChartBarSquare" iconName="Dashboard" to="" />
+      <Route icon="Banknotes" iconName="Transection" to="transection" />
+      <Route icon="UserIcon" iconName="Profile" to="profile" />
     </div>
   );
 };
-const Route = ({ selected, icon, iconName }) => {
+
+const Route = ({ icon, iconName, to }) => {
   return (
-    <button
-      className={`${
-        selected
-          ? "bg-white text-stone-950 shadow-lg"
-          : "text-stone-500 hover:bg-stone-200 shadow-none"
-      } flex items-center space-x-3 w-full transition-all py-3 px-2 rounded-xl cursor-pointer`}
+    <NavLink
+      to={to}
+      end
+      className={({ isActive }) =>
+        isActive
+          ? "flex items-center space-x-3 w-full bg-white text-stone-950 shadow-lg transition-all py-3 px-2 rounded-xl cursor-pointer"
+          : "flex items-center space-x-3 w-full text-stone-500 hover:bg-stone-200 shadow-none transition-all py-3 px-2 rounded-xl cursor-pointer"
+      }
     >
       {icon === "ChartBarSquare" && <ChartBarSquareIcon className="w-7 h-7" />}
       {icon === "Banknotes" && <BanknotesIcon className="w-7 h-7" />}
       {icon === "UserIcon" && <UserIcon className="w-7 h-7" />}
+      {icon === "Product" && <ShoppingBagIcon className="w-7 h-7" />}
       <p className="text-xl tracking-wider mt-1">{iconName}</p>
-    </button>
+    </NavLink>
   );
 };
+
 export default RouteSelect;
