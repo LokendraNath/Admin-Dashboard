258a951cf2c4cd0862eb294a955962314f4fd57e
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-03 15:42:47 +0530

    move account toggle to bottom

--- src/components/Sidebar/AccountToggle.tsx
+++ src/components/Sidebar/AccountToggle.tsx
@@ -1,27 +1,31 @@
 import React from "react";
-import { EllipsisHorizontalIcon } from "@heroicons/react/16/solid";
+import {
+  ChevronDoubleLeftIcon,
+  ChevronDoubleRightIcon,
+  ChevronRightIcon,
+  EllipsisHorizontalIcon,
+  ForwardIcon,
+} from "@heroicons/react/16/solid";
 import profileImage from "@assets/pfp-lokendra2.png";
 export const AccountToggle = () => {
   return (
-    <div className="mt-4 border-b border-stone-300 pb-2 mb-4">
-      <button className="flex gap-2 hover:bg-stone-200 w-full rounded-full p-2 transition-colors px-2 duration-200 items-center justify-between cursor-pointer">
-        <div className="flex space-x-2 items-center">
-          <img
-            className="w-12 h-12 rounded-full shrink-0 shadow"
-            src={profileImage}
-            alt="Profile"
-          />
-          <div className="text-start">
-            <span className="text-[17px] font-bold block tracking-wide">
-              Lokendra Nath
-            </span>
-            <span className="text-sm font-normal block tracking-wider text-stone-400">
-              ln25@gmail.com
-            </span>
-          </div>
+    <div className="flex mt-4 border-stone-300 pb-2 p-4 space-x-3">
+      <button className="flex w-full rounded-full p-1 transition-colors  duration-200 cursor-pointer space-x-2 hover:bg-stone-100">
+        <img
+          className="w-10 h-10 rounded-full shrink-0 shadow"
+          src={profileImage}
+          alt="Profile"
+        />
+        <div className="text-start">
+          <span className="text-[14px] font-bold block tracking-wide">
+            Lokendra Nath
+          </span>
+          <span className="text-[11px] font-normal block tracking-wider text-stone-400">
+            ln25@gmail.com
+          </span>
         </div>
-        <EllipsisHorizontalIcon className="w-5 text-gray-400 ml-2 rounded-full" />
       </button>
+      <ChevronDoubleLeftIcon className="cursor-pointer" width={25} />
     </div>
   );
 };
