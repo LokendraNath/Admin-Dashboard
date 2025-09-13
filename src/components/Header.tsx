3715cc7f698e9f43407b0557f65dcee10e8c620d
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-13 17:12:12 +0530

    header darkmode button add

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,38 @@
 import { useContext } from "react";
 import { SidebarContext } from "../Layout";
-import { ChevronDoubleRightIcon } from "@heroicons/react/16/solid";
+import {
+  ChevronDoubleRightIcon,
+  MoonIcon,
+  SunIcon,
+} from "@heroicons/react/16/solid";
 import profileImage from "/assets/pfp-lokendra2.png";
+import { useThemeStore } from "@/store/store";
 
 const Header = () => {
   const { expanded, setExpanded } = useContext(SidebarContext);
+  const { theme, toggleTheme } = useThemeStore();
 
   return (
-    <div className="sticky top-0 w-full h-15 lg:h-17 flex overflow-hidden z-40 bg-white ow-lg px-10 items-center justify-between">
+    <div className="sticky top-0 w-full h-15 lg:h-17 flex overflow-hidden z-40 bg-white dark:bg-[#1D202B] ow-lg px-10 items-center justify-between dark:text-white">
       <ChevronDoubleRightIcon
         className="cursor-pointer lg:hidden"
         width={30}
         onClick={() => setExpanded(!expanded)}
       />
       <h1 className="text-2xl font-revalia">CapitalEdge</h1>
-      <img
-        src={profileImage}
-        className="h-10 w-10 object-cover rounded-full"
-        alt=""
-      />
+      {theme === "light" ? (
+        <MoonIcon
+          className="cursor-pointer"
+          onClick={() => toggleTheme}
+          width={25}
+        />
+      ) : (
+        <SunIcon
+          className="cursor-pointer"
+          onClick={() => toggleTheme}
+          width={25}
+        />
+      )}
     </div>
   );
 };
